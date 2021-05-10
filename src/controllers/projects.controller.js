0ea24df75160b3a82a670e8676fbb150707abc12
d-bizari <daniel.bizari@mercadolibre.com>
2021-05-09 22:37:29 -0300

    set pool config for db

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,6 +1,16 @@
 const { Pool } = require('pg')
 
-const getProject = (req, res) => {
+const pool = new Pool({
+    host: 'psql-container',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'firsttest',
+    port: '5432'
+})
+
+const getProject = async (req, res) => {
+    const response = await pool.query('SELECT * FROM projects');
+    console.log(response.rows);
     res.send('proyecto re loco capo')
 }
 
