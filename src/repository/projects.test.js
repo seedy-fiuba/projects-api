234036729f8171497007dd91d5c8caa1999fdd23
d-bizari <daniel.bizari@mercadolibre.com>
2021-05-25 01:11:06 -0300

    applied lint

--- src/repository/projects.test.js
+++ src/repository/projects.test.js
@@ -1,78 +1,78 @@
 const projectRepository = require('./projects');
-let projectModel = require('../model/projects')
+let projectModel = require('../model/projects');
 let mockingoose = require('mockingoose');
 
 describe('get project by id', () => {
-    mockingoose(projectModel)
+	mockingoose(projectModel);
 
-    beforeEach(() => {
+	beforeEach(() => {
 
-    });
+	});
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
 
-    test('should return project for id 123', async () => {
-        let projectDoc = {
-            _id: 123,
-            name: 'boquita',
-            description: 'dale bokeee'
-        };
+	test('should return project for id 123', async () => {
+		let projectDoc = {
+			_id: 123,
+			name: 'boquita',
+			description: 'dale bokeee'
+		};
 
-        mockingoose(projectModel).toReturn(projectDoc, 'findOne');
+		mockingoose(projectModel).toReturn(projectDoc, 'findOne');
 
-        const projectResult = await projectRepository.getProjectByid('123');
+		const projectResult = await projectRepository.getProjectByid('123');
 
-        expect(projectResult).toMatchObject(projectDoc);
-    });
+		expect(projectResult).toMatchObject(projectDoc);
+	});
 
-    test('should return project for id 456 with query spy', async () => {
-        let projectDoc = {
-            _id: 456,
-            name: 'boquita',
-            description: 'dale bokeee'
-        };
+	test('should return project for id 456 with query spy', async () => {
+		let projectDoc = {
+			_id: 456,
+			name: 'boquita',
+			description: 'dale bokeee'
+		};
 
-        const finderMock = query => {
-            expect(query.getQuery()).toMatchSnapshot('findById query');
+		const finderMock = query => {
+			expect(query.getQuery()).toMatchSnapshot('findById query');
 
-            if (query.getQuery()._id === '456') {
-                return projectDoc;
-            }
-        };
+			if (query.getQuery()._id === '456') {
+				return projectDoc;
+			}
+		};
 
-        mockingoose(projectModel).toReturn(finderMock, 'findOne'); // findById is findOne
+		mockingoose(projectModel).toReturn(finderMock, 'findOne'); // findById is findOne
 
-        const projectResult = await projectRepository.getProjectByid('456');
+		const projectResult = await projectRepository.getProjectByid('456');
 
-        expect(projectResult).toMatchObject(projectDoc);
-    });
+		expect(projectResult).toMatchObject(projectDoc);
+	});
 });
 
 describe('POST project', () => {
-    mockingoose(projectModel)
+	mockingoose(projectModel);
 
-    beforeEach(() => {
-        mockingoose(projectModel).reset(); // will reset all operations;
-    });
+	beforeEach(() => {
+		mockingoose(projectModel).reset(); // will reset all operations;
+	});
 
-    afterEach(() => {
-        jest.clearAllMocks();
+	afterEach(() => {
+		jest.clearAllMocks();
 
-    });
+	});
 
-    test('should create project id 123', async () => {
-        let projectDoc = {
-            _id: 123,
-            name: 'boquita',
-            description: 'dale bokeee'
-        };
+	test('should create project id 123', async () => {
+		let projectDoc = {
+			_id: 123,
+			name: 'boquita',
+			description: 'dale bokeee'
+		};
 
-        mockingoose(projectModel).toReturn(projectDoc, 'save');
+		mockingoose(projectModel).toReturn(projectDoc, 'save');
 
-        const projectResult = await projectRepository.createProject('boquita', 'dale bokeee');
+		const projectResult = await projectRepository.createProject('boquita', 'dale bokeee');
 
-        expect(projectResult).toMatchObject(projectDoc);
-    });
+		expect(projectResult).toMatchObject(projectDoc);
+	});
 });
