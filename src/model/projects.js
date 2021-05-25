9e0de66686a67454a1898a08636fb7d6298b4b53
d-bizari <daniel.bizari@mercadolibre.com>
2021-05-25 14:12:51 -0300

    defined model of projects

--- src/model/projects.js
+++ src/model/projects.js
@@ -5,8 +5,27 @@ let Schema = mongoose.Schema;
 
 let projectSchema = new Schema({
 	_id: Number,
-	name: { type: String, required: true},
-	description: { type: String, required: true}
+	title: { type: String, required: true},
+	description: { type: String, required: true},
+	category: {type: String, required: true},
+	status: {type: String, required: true},
+	mediaUrls: {type: [String], required: true},
+	targetAmount: {type: Number, required: true},
+	fundedAmount: {type: Number, required: true},
+	location: {
+		type: {
+			type: String,
+			enum: ['Point'], // 'location.type' must be 'Point'
+			required: true
+		},
+		coordinates: {
+			type: [Number],
+			required: true
+		},
+		index: '2dsphere' // Create a special 2dsphere index this is required for $near and $geoNear operator
+	},
+	hashtags: {type: [String], required: false},
+
 }, { timestamps: true, _id: false  }); // timestamps adds "createdAt" and "updatedAt" fields
 
 if (process.env.SCOPE === 'PROD') {
