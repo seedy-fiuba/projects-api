b3282d1db5a2a3974c0566dca92949c9c38603e2
d-bizari <daniel.bizari@mercadolibre.com>
2021-06-17 23:08:02 -0300

    added tests to increase coverage

--- test/project.test.js
+++ test/project.test.js
@@ -2,6 +2,11 @@ const supertest = require('supertest');
 const server = require('../src/server');
 let request = supertest(server.app);
 let projectMockRepository = require('../src/repository/projects');
+let authenticationMock = require('../src/client/authentication')
+
+jest.mock('../src/client/authentication', () => {
+	return {authenticateToken: jest.fn()}
+})
 
 jest.mock('../src/repository/projects', () => {
 	return {
@@ -16,7 +21,7 @@ jest.mock('../src/repository/projects', () => {
 describe('POST /api/project', () => {
 	beforeEach(() => {
 		projectMockRepository.createProject.mockReset();
-		// projectMockRepository.mockResolvedValue(0)
+		authenticationMock.authenticateToken.mockReset()
 	});
 
 	afterAll(() => {
@@ -96,6 +101,168 @@ describe('POST /api/project', () => {
 		expect(res.status).toBe(200);
 		expect(res.body).toStrictEqual(doc);
 	});
+
+	test('create a new project use ownerId from token', async () => {
+		let tomorrow = new Date()
+		tomorrow.setDate(tomorrow.getDate() + 1)
+
+		let body = {
+			title: 'pad gamer',
+			description: 'teclado gamer rgb con muchas luces',
+			category: 'gamer',
+			mediaUrls: ['foto/fachera'],
+			location: {
+				x: -34.610955,
+				y: -58.436967
+			},
+			stages: [
+				{
+					track: "armado",
+					targetAmount: 12.22
+				},
+				{
+					track: "distribucion",
+					targetAmount: 125.22
+				}
+			],
+			finishDate: tomorrow.toISOString(),
+			hashtags: ['gamer', 'rgb', 'mecanico']
+		};
+
+		let doc = {
+			_id: 123,
+			title: 'pad gamer',
+			description: 'teclado gamer rgb con muchas luces',
+			category: 'gamer',
+			mediaUrls: ['foto/fachera'],
+			fundedAmount: 0.0,
+			location: {
+				coordinates: [
+					-34.610955,
+					-58.436967
+				],
+				type: 'Point'
+			},
+			hashtags: ['gamer', 'rgb', 'mecanico'],
+			stages: [
+				{
+					track: "armado",
+					targetAmount: 12.22
+				},
+				{
+					track: "distribucion",
+					targetAmount: 125.22
+				}
+			],
+			finishDate: tomorrow.toISOString(),
+			ownerId: 555,
+			reviewerId: 0,
+			status: 'created',
+		};
+
+		projectMockRepository.createProject.mockReturnValueOnce(doc);
+		authenticationMock.authenticateToken.mockReturnValueOnce({
+			message: 'authorized',
+			identity: {
+				id: 555
+			}
+		})
+
+		const res = await request.post('/api/project').set('X-auth-Token','asdfasdfasdfs').send(body);
+
+		expect(projectMockRepository.createProject.mock.calls.length).toBe(1);
+		expect(authenticationMock.authenticateToken.mock.calls.length).toBe(1);
+
+		// payload for project creation
+		body.status = 'created'
+		body.reviewerId = 0
+		body.ownerId = 555
+		body.finishDate = tomorrow
+		expect(projectMockRepository.createProject.mock.calls[0][0]).toStrictEqual(body);
+		expect(res.status).toBe(200);
+		expect(res.body).toStrictEqual(doc);
+	});
+
+	test('create a new project with reviewerId', async () => {
+		let tomorrow = new Date()
+		tomorrow.setDate(tomorrow.getDate() + 1)
+
+		let body = {
+			title: 'pad gamer',
+			description: 'teclado gamer rgb con muchas luces',
+			category: 'gamer',
+			mediaUrls: ['foto/fachera'],
+			location: {
+				x: -34.610955,
+				y: -58.436967
+			},
+			stages: [
+				{
+					track: "armado",
+					targetAmount: 12.22
+				},
+				{
+					track: "distribucion",
+					targetAmount: 125.22
+				}
+			],
+			reviewerId: 223,
+			finishDate: tomorrow.toISOString(),
+			hashtags: ['gamer', 'rgb', 'mecanico']
+		};
+
+		let doc = {
+			_id: 123,
+			title: 'pad gamer',
+			description: 'teclado gamer rgb con muchas luces',
+			category: 'gamer',
+			mediaUrls: ['foto/fachera'],
+			fundedAmount: 0.0,
+			location: {
+				coordinates: [
+					-34.610955,
+					-58.436967
+				],
+				type: 'Point'
+			},
+			hashtags: ['gamer', 'rgb', 'mecanico'],
+			stages: [
+				{
+					track: "armado",
+					targetAmount: 12.22
+				},
+				{
+					track: "distribucion",
+					targetAmount: 125.22
+				}
+			],
+			finishDate: tomorrow.toISOString(),
+			ownerId: 555,
+			reviewerId: 223,
+			status: 'created',
+		};
+
+		projectMockRepository.createProject.mockReturnValueOnce(doc);
+		authenticationMock.authenticateToken.mockReturnValueOnce({
+			message: 'authorized',
+			identity: {
+				id: 555
+			}
+		})
+
+		const res = await request.post('/api/project').set('X-auth-Token','asdfasdfasdfs').send(body);
+
+		expect(projectMockRepository.createProject.mock.calls.length).toBe(1);
+		expect(authenticationMock.authenticateToken.mock.calls.length).toBe(1);
+
+		// payload for project creation
+		body.status = 'in-progress'
+		body.ownerId = 555
+		body.finishDate = tomorrow
+		expect(projectMockRepository.createProject.mock.calls[0][0]).toStrictEqual(body);
+		expect(res.status).toBe(200);
+		expect(res.body).toStrictEqual(doc);
+	});
 });
 
 describe('GET /api/project', () => {
@@ -127,6 +294,27 @@ describe('GET /api/project', () => {
 			description: 'que te importa'
 		});
 	});
+
+	test('get project id 123 not found', async () => {
+
+		projectMockRepository.getProjectByid.mockReturnValueOnce(null);
+
+		const res = await request.get('/api/project/123').set('X-Override-Token','true');
+
+		expect(projectMockRepository.getProjectByid.mock.calls.length).toBe(1);
+		expect(projectMockRepository.getProjectByid.mock.calls[0][0]).toBe('123');
+
+		expect(res.status).toBe(404);
+		expect(res.body.message).toContain('inexistent project');
+	});
+});
+
+describe('PUT /api/project/{projectId}', () => {
+	beforeEach(() => {
+		projectMockRepository.getProjectByid.mockReset()
+		projectMockRepository.updateProject.mockReset()
+		authenticationMock.authenticateToken.mockReset()
+	})
 
 	test('update project id 456', async () => {
 		let body = {
@@ -191,6 +379,66 @@ describe('GET /api/project', () => {
 		expect(res.body).toStrictEqual(projectUpdated);
 	});
 
+	test('update project with reviewerId change status to in-progress', async () => {
+		let body = {
+			reviewerId: 2342
+		};
+
+		let oldProject = {
+			_id: 123,
+			title: 'pad gamer re loco',
+			description: 'teclado gamer rgb con pocas luces',
+			category: 'rgb',
+			mediaUrls: ['foto/fachera'],
+			targetAmount: 123.22,
+			fundedAmount: 0.0,
+			status: 'created',
+			reviewerId: 0,
+			location: {
+				coordinates: [
+					-34.610955,
+					-58.436967
+				],
+				type: 'Point'
+			},
+			hashtags: ['gamer', 'rgb', 'mecanico']
+		};
+
+		let projectUpdated = {
+			_id: 123,
+			title: 'pad gamer re loco',
+			description: 'teclado gamer rgb con pocas luces',
+			category: 'rgb',
+			mediaUrls: ['foto/fachera'],
+			targetAmount: 123.22,
+			fundedAmount: 0.0,
+			location: {
+				coordinates: [
+					-34.610955,
+					-58.436967
+				],
+				type: 'Point'
+			},
+			reviewerId: 2342,
+			hashtags: ['gamer', 'rgb', 'mecanico']
+		};
+
+		projectMockRepository.getProjectByid.mockReturnValueOnce(oldProject);
+		projectMockRepository.updateProject.mockReturnValueOnce(projectUpdated);
+
+		const res = await request.put('/api/project/456').set('X-Override-Token','true').send(body);
+
+		expect(projectMockRepository.getProjectByid.mock.calls.length).toBe(1);
+		expect(projectMockRepository.getProjectByid.mock.calls[0][0]).toBe('456');
+		expect(projectMockRepository.updateProject.mock.calls.length).toBe(1);
+		expect(projectMockRepository.updateProject.mock.calls[0][0]).toBe('456');
+
+		body.status = 'in-progress'
+		expect(projectMockRepository.updateProject.mock.calls[0][1]).toStrictEqual(body);
+		expect(res.status).toBe(200);
+		expect(res.body).toStrictEqual(projectUpdated);
+	});
+
 	test('update project id 456 fails due to db unavailable', async () => {
 
 		let body = {
@@ -216,6 +464,17 @@ describe('GET /api/project', () => {
 
 		expect(res.status).toBe(500);
 		expect(res.text).toContain('database unavailable');
+
+	});
+
+	test('fail update with empty body', async () => {
+
+		let body = {};
+
+		const res = await request.put('/api/project/456').set('X-Override-Token','true').send(body);
+
+		expect(res.status).toBe(400);
+		expect(res.text).toContain('at least one field is required to update');
 
 	});
 });
